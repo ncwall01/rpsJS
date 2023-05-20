bcc04d2fb8fff39f2ccebf5455bf1915c91aadee
Noah Wall <104470013+ncwall01@users.noreply.github.com>
2023-05-20 11:20:34 -0400

    Add files via upload

--- starter.js
+++ starter.js
@@ -1,64 +1,74 @@
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase(); 
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
-        return userInput
-    } else {
-        console.log('Error!')
-    }
+let userScore = 0;
+let compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+
+document.getElementById('rock').onclick = user;
+document.getElementById('paper').onclick = user;
+document.getElementById('scissors').onclick = user;
+
+function user() {
+  var userChoice = this.id;
+  document.getElementById("user").innerHTML = "User threw: " + userChoice;
+  console.log("User threw: " + userChoice);
+
+  var computerChoice = Math.random();
+  if (computerChoice < 0.34) {
+    computerChoice = "rock";
+  } else if (computerChoice <= 0.67) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  document.getElementById("comp").innerHTML = "Computer threw: " + computerChoice;
+  console.log("Computer threw: " + computerChoice);
+
+  console.log(compare(userChoice, computerChoice));
 }
 
-const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
+function compare(choice1, choice2) {
+  if (choice1 === choice2) {
+    return "The result is a tie!";
+  }
+
+  if (choice1 === "rock") {
+    if (choice2 === "scissors") {
+      win();
+      return "rock wins";
+    } else {
+      lose();
+      return "paper wins";
     }
-};
+  }
 
-const determineWinner = (userChoice, computerChoice) => {
-    if (userChoice === computerChoice) {
-        return 'This game is a tie!';
+  if (choice1 === "paper") {
+    if (choice2 === "rock") {
+      win();
+      return "paper wins";
+    } else {
+      lose();
+      return "scissors wins";
     }
-    if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-            return 'Sorry, computer won!';
-        } else {
-            return 'Congrats, you won!';
-        }
+  }
+
+  if (choice1 === "scissors") {
+    if (choice2 === "rock") {
+      lose();
+      return "rock wins";
+    } else {
+      win();
+      return "scissors wins";
     }
+  }
+}
 
-    if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-            return 'Sorry, computer won!';
-        } else {
-            return 'Congrats, you won!';
-        }
-    }
+function win() {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+}
 
-    if (userChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-            return 'Sorry, computer won!';
-        } else {
-            return 'Congrats, you won!';
-        }
-    }
-
-    if (userChoice === 'bomb') {
-        return 'Congrats, you won!';
-    }
-};
-
-const playGame = () => {
-    const userChoice = getUserChoice('rock');
-    const computerChoice = getComputerChoice();
-    console.log('You threw: ' + userChoice);
-    console.log('Computer threw: ' + computerChoice);
-
-    console.log(determineWinner(userChoice, computerChoice));
-};
-
-playGame()
+function lose() {
+  compScore++;
+  compScore_span.innerHTML = compScore;
+}
